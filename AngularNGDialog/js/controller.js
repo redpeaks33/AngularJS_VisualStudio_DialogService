085495c1f77958322851643d2b619a99f9251694
redpeaks33 <redpeaks33@gmail.com>
2016-04-03 06:56:37 +0900

    NGDialogSercvice
    NGDialogSercvice

---
+++ AngularNGDialog/js/controller.js
@@ -0,0 +1,10 @@
+﻿var main = angular.module("app", ['ui.bootstrap','dialogs.main']);
+
+main.controller('MyCtrl', function ($scope,dialogs) {
+    $scope.Open = function () {
+        dialogs.notify('Something Happened', 'Something happened at this point in the application that I wish to let you know about');
+        //dialogs.error('Error', 'An unknown error occurred preventing the completion of the requested action.');
+        //dialogs.wait('Creating User', 'Please wait while we attempt to create user "Michael Conroy."<br><br>This should only take a moment.', 50);
+    }
+});
+
